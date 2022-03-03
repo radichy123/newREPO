9871a9441137024af47116605fab50227d79a91d
radichy123 <radi_khan25@yahoo.com>
2022-03-03 18:04:36 -0500

    added project

---
+++ Gitpractice/src/main/java/com/BaseTest.java
@@ -0,0 +1,21 @@
+package com;
+
+public class BaseTest {
+	///nothing changed
+
+	
+	public void name() {
+		 ///changing something 
+		///1111
+		///222
+		///33333
+		
+		
+		////444444\\\\\\\\\
+		/////55555\\\\\
+		/////666666\\\\\
+		
+		
+		///many things changed
+	}
+}
